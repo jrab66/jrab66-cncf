0bb1257b53b30661a3346bbb07d914ecd0d60113
jrab66 <jrab6692@gmail.com>
2024-09-25 16:53:48 -0600

    feat: testing logging app

--- github-gke/codebase/server.js
+++ github-gke/codebase/server.js
@@ -6,11 +6,20 @@ const express = require('express');
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hola Mundo Express 1\n');
 
+
+// App
+
+const app = express();
+var morgan = require('morgan');
+
+
+app.use(morgan('combined'));
+
+app.get('/', function (req, res) {
+  res.send('hello, world express');
 });
+
+
 app.listen(PORT, HOST);
 console.log(`Corriendo en://${HOST}:${PORT}`);
